02156a20b174c8d774bf5c94eb1d65b378520e35
MomoCyann <541525127@qq.com>
2021-09-09 12:48:36 +0800

    update

--- SRCPSP.py
+++ SRCPSP.py
@@ -11,7 +11,7 @@ import copy
 #   再判定资源够不够，
 #     能进行就抛入 正在进行 ，记录starttime，资源数量 减少相应的r1[i] r2[i]，判断下一个工序
 #     不够就推迟时间一个单位，
-#       时间推迟之后 判断 正在进行 列表内的工序是否完成， 判定此时时间是否是starttime+ duration
+#todo     时间推迟之后 判断 正在进行 列表内的工序是否完成， 判定此时时间是否是starttime+ duration
 #         等于就标记完成，抛入已进行。资源量+=r1[i]
 #         此刻检查待进行里的每一个工序
 #           检查紧前
@@ -27,7 +27,7 @@ data = {
     4:[8],
     5:[9],
     6:[10],
-    7:[11],
+    7:[10],
     8:[11],
     9:[11],
     10:[12],
@@ -35,7 +35,7 @@ data = {
     12:[],
 }
 duration = [0,3,4,2,6,3,2,3,4,3,2,0]
-src = {
+src = [
     [0,0],
     [2,0],
     [1,0],
@@ -47,17 +47,47 @@ src = {
     [0,1],
     [3,0],
     [0,2],
-    [0,0],
-}
+    [0,0]
+]
+suc_proc=[1]
+wat_proc=[]
+now_proc=[]
+r1=3
+r2=4
+time = 0
+#设置参数
+# min_x = 0         #变量范围
+# max_x = 10
+gen_num = 20   #迭代次数
+popu_num = 10    #种群大小
+p_mut = 0.05      #突变概率
+#mut_wet = 0.5     #突变权重
+p_cro = 0.5      #重组概率
+proc_num = 12     #染色体长度
+
+#创建空数组(种群)
+popu = []
+for i in range(popu_num):
+        temp = []
+        popu.append(temp)
+#适应度
+fitness = []
+#选择概率
+p = []
+#答案
+best_fit = 0
+best_chromo = 0
 #初始化
 def initialize():
-    #向空数组中加入不重复的随机数，表示城市顺序。
+    #向空数组中加入不重复的随机数，表示顺序。
     for i in range(popu_num):
         #先按顺序生成城市个数的整数
         temp = []
-        for j in range(1,proc_num+1):    #题目数字是1 到12
+        for j in range(2,proc_num):    #题目数字是1 到12 这里只加入2到11
             temp.append(j)
         random.shuffle(temp)
+        temp.insert(0,1)
+        temp.append(12)
         #向种群加入打乱顺序的整数，表示随机顺序。
         for k in range(proc_num):
             popu[i].append(temp[k])
@@ -65,17 +95,51 @@ def initialize():
 popu = initialize()
 print(popu)
 
-def fitnessfunction():
-    fitness.clear()
-    starttime=0
-    endtime=0
+#适应度计算
+#def fitnessfunction():
+fitness.clear()
+starttime=0
+endtime=0
 
-    for i in range(len(popu)):
-        d = 0
-        for j in range(len(popu[i])-1):
+for i in range(len(popu)):
+    for j in popu[i]:#每个工序
+        if j == 1:
+            suc_proc.append(j)
+            continue
+        for k,v in data.items():
+            #判断紧前工序
+            if j in v:
+                if k not in suc_proc:#紧前工序还没完成
+                    wat_proc.append(j)
+                    continue
+                else:
+                    if j in wat_proc:#有可能存在另一个已完成的紧前工序
+                        wat_proc.remove(j)
+                        now_proc.append(j)
+                    now_proc.append(j)
+                    break
+        if now_proc:
+            if r1 > src[j-1][0] and r2 > src[j-1][1]: #判断资源
+                starttime = time
+                endtime = starttime + duration[j]
+                r1 -= src[j-1][0]
+                r2 -= src[j-1][1]
+                continue
+            else:
+                time+=1
 
-        fitness.append(1/d)
-    return fitness
+        else:
+            continue
+
+
+
+
+
+
+
+
+    #return fitness
+
 
 #选择
 def choose():
@@ -140,42 +204,6 @@ def crossover():
             continue
     return popu
 
-#设置参数
-# min_x = 0         #变量范围
-# max_x = 10
-gen_num = 20   #迭代次数
-popu_num = 10    #种群大小
-p_mut = 0.05      #突变概率
-#mut_wet = 0.5     #突变权重
-p_cro = 0.5      #重组概率
-data = {
-    1:[2,3,4,5],
-    2:[6],
-    3:[7],
-    4:[8],
-    5:[9],
-    6:[10],
-    7:[11],
-    8:[11],
-    9:[11],
-    10:[12],
-    11:[12],
-    12:[],
-}
-proc_num = 12     #染色体长度
-
-#创建空数组(种群)
-popu = []
-for i in range(popu_num):
-        temp = []
-        popu.append(temp)
-#适应度
-fitness = []
-#选择概率
-p = []
-#答案
-best_fit = 0
-best_chromo = 0
 
 
 
